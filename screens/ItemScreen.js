d0606c3b3ced86db48bbb87b60a86e63cbdb1562
Talent <kasame_suk@hotmail.com>
2019-11-04 11:04:10 +0700

    Update ItemScreen.js

--- screens/ItemScreen.js
+++ screens/ItemScreen.js
@@ -1,5 +1,6 @@
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
+// import * as firebase from 'firebase';
 import {
   Image,
   Platform,
@@ -11,6 +12,9 @@ import {
 } from 'react-native';
 
 import Images from '../helper/imageHelper'
+import FirebaseHelper from '../helper/FireBaseHelper'
+// import FirebaseHelper2 from '../helper/FirebaseHelper2'
+import getData from '../helper/FirebaseHelper2';
 
 export default class ItemScreen extends React.Component {
 
@@ -22,14 +26,41 @@ export default class ItemScreen extends React.Component {
   //   tabBarIcon: null,
   //  };
 
+  componentDidMount() {
+
+    // let obj = FirebaseHelper.readUserData("tb_product_master");
+
+    console.log("onload")
+    // console.log(FirebaseHelper.readUserData("tb_product_master"))
+    // console.log(FirebaseHelper2.getData("tb_product_master"));
+    FirebaseHelper.readUserData("tb_product_master").then((data)=>{
+      console.log("========= API ============")
+      console.log(data)
+    })
+
+    // console.log(getData("tb_product_master"))
+
+  }
+
   render(){
     return (
     
-        <View style={styles.container}>
-            <Text style={{textAlign: 'center', fontSize: 20}}>
-                รายการสินค้า
-            </Text>
+      <View style={styles.container}>
+        <View style={styles.imagePanel}>
+          <View style={styles.imageGrid}>
+            <Image 
+              source={Images.productTest} 
+              style={
+                {
+                  width: '100%',
+                  height: '100%',
+                }
+              }
+              resizeMode='contain' 
+            />
+          </View>
         </View>
+      </View>
   
     );
   }
@@ -38,8 +69,6 @@ export default class ItemScreen extends React.Component {
 
 ItemScreen.navigationOptions = {
   title: 'รายการสินค้า',
-  // header: null,
-  // tabBarVisible: false
 };
 
 const styles = StyleSheet.create({
@@ -47,5 +76,19 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     paddingHorizontal: 10
+  },
+  imagePanel: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  imageGrid: {
+    width: '80%',
+    height: '80%',
+    borderColor: '#000000',
+    borderWidth: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10
   },
 });
