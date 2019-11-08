2dcb62afd4c95c307fd893b7f2e168090997f8c2
Talent <kasame_suk@hotmail.com>
2019-11-08 15:43:39 +0700

    Update BookingScreen.js

--- screens/BookingScreen.js
+++ screens/BookingScreen.js
@@ -2,10 +2,8 @@ import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import {
   Image,
-  Platform,
   StyleSheet,
   Text,
-  TouchableOpacity,
   TextInput,
   View,
   Alert,
