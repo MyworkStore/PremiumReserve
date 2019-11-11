2d5bd1109d47118280537b1e7fc4cd0e496b1cf0
Talent <kasame_suk@hotmail.com>
2019-11-11 17:14:24 +0700

    Update PushNotificationScreen.js

--- screens/PushNotificationScreen.js
+++ screens/PushNotificationScreen.js
@@ -6,7 +6,8 @@ import {
     TextInput,
     Text,
     Keyboard,
-    ScrollView
+    ScrollView,
+    ActivityIndicator
 } from 'react-native'
 
 import Constants from 'expo-constants';
@@ -19,6 +20,7 @@ export default class PushNotificationScreen extends React.Component{
     constructor (props) {
         super(props);
         this.state = {
+          loadToken: false,
           token: "",
           message: "",
         };
@@ -26,36 +28,66 @@ export default class PushNotificationScreen extends React.Component{
       }
 
     getToken = async() => {
+
+        this.setState({
+            loadToken: true
+        })
+
         const { status: existingStatus } = await Permissions.getAsync(
             Permissions.NOTIFICATIONS
-          );
-          let finalStatus = existingStatus;
+        );
+        let finalStatus = existingStatus;
 
-          if (existingStatus !== 'granted') {
-            const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-            finalStatus = status;
-          }
-        
-          if (finalStatus !== 'granted') {
-            return;
-          }
-        
-          let token = await Notifications.getExpoPushTokenAsync();
+        if (existingStatus !== 'granted') {
+        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+        finalStatus = status;
+        }
+    
+        if (finalStatus !== 'granted') {
+        return;
+        }
+    
+        let token = await Notifications.getExpoPushTokenAsync();
 
-          this.setState({
-              token: token
-          })
+        this.setState({
+            token: token
+        })
 
-          const param = {
-              token: this.state.token
-          }
+        if( this.state.token != "" ){
+            this.setState({
+                loadToken: false
+            })
+        }
 
-          FirebaseHelper.update('/userNoti/hok', param)
+        const param = {
+            token: this.state.token
+        }
+
+        FirebaseHelper.update('/userNoti/hok', param)
 
     }
 
     sendMessage = () => {
         Keyboard.dismiss()
+
+        let response = fetch('https://exp.host/--/api/v2/push/send', {
+            method: 'POST',
+            header: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                to: 'ExponentPushToken[47dZcMOyoO_tyD97iyunKN]',
+                sound: 'default',
+                title: 'ทดสอบ Notification',
+                body: this.state.message
+                
+            })
+        })
+
+        console.log("========== Response ===========")
+        console.log(response)
+
     }
 
     render(){
@@ -63,29 +95,37 @@ export default class PushNotificationScreen extends React.Component{
             <View style={styles.container}>
                 <ScrollView keyboardShouldPersistTaps='handled'>
                     <View>
-                        <TouchableOpacity style={{backgroundColor: '#3385ff', marginTop: 10, height: 50, alignItems: 'center', justifyContent: 'center'}}
+                        <TouchableOpacity style={{backgroundColor: '#33bbff', marginTop: 10, height: 50, alignItems: 'center', justifyContent: 'center'}}
                             onPress={this.getToken}
                         >
                             <Text style={{textAlign: 'center'}}>
                                 กดเพื่อรับ token
                             </Text>
                         </TouchableOpacity>
-                        <Text>
-                            {this.state.token}
-                        </Text>
+                        {
+                            this.state.loadToken == true  ? <ActivityIndicator size="large" color="#0000ff" /> :
+                            <Text style={{textAlign: 'center', marginTop: 40}}>
+                                {this.state.token}
+                            </Text>
+                        }
                     </View>
-                    {/* <TextInput 
-                        style={{borderColor: '#000000', borderWidth: 1, padding: 10}} 
-                        placeholder='ใส่ข้อความ Noti'
-                        onChangeText={(text)=>{this.setState({message: text})}}
-                    />
-                    <TouchableOpacity style={{backgroundColor: '#00e600', marginTop: 10, height: 50, alignItems: 'center', justifyContent: 'center'}}
-                        onPress={this.sendMessage}
-                    >
-                        <Text style={{textAlign: 'center'}}>
-                            กดเพื่อส่งข้อความ
-                        </Text>
-                    </TouchableOpacity> */}
+                    {
+                        this.state.token == "" ? <View></View> :
+                        <View style={{marginTop: 20}}>
+                            <TextInput 
+                                style={{borderColor: '#000000', borderWidth: 1, padding: 10}} 
+                                placeholder='ใส่ข้อความ Noti'
+                                onChangeText={(text)=>{this.setState({message: text})}}
+                            />
+                            <TouchableOpacity style={{backgroundColor: '#00e600', marginTop: 10, height: 50, alignItems: 'center', justifyContent: 'center'}}
+                                onPress={this.sendMessage}
+                            >
+                                <Text style={{textAlign: 'center'}}>
+                                    กดเพื่อส่งข้อความ
+                                </Text>
+                            </TouchableOpacity>
+                        </View>
+                    }
                 </ScrollView>
             </View>
         )
